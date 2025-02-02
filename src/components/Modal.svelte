dddd2ce5230ceee15eba374b83fc97ea4e7c6333
Xurify <52548195+Xurify@users.noreply.github.com>
2025-02-01 22:04:33 -0500

    Migrated to Taliwind v4

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -3,7 +3,7 @@
 </script>
 
 {#if show}
-	<div class="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/60">
+	<div class="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs bg-black/60">
 		<div class="mx-4 w-full max-w-xl rounded-lg border border-white/20 bg-[#141612] p-8 shadow-xl">
 			<div class="text-center">
 				<div class="flex flex-col justify-center gap-4 text-2xl font-semibold text-red-500">

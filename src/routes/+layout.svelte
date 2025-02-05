c17cfccb269a75ca5573c7faf73ad31197d9b3bb
Xurify <52548195+Xurify@users.noreply.github.com>
2025-02-05 00:34:05 -0500

    Added @vercel/analytics/sveltekit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.css';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { dev } from '$app/environment';
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot></slot>

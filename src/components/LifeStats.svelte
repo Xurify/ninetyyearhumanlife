ab2f04b345815dc2bc947c994dd0297d62610848
Xurify <52548195+Xurify@users.noreply.github.com>
2025-05-27 13:03:38 -0400

    Updated text

--- src/components/LifeStats.svelte
+++ src/components/LifeStats.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="flex flex-col gap-1 text-center text-sm">
-	<p class="text-orange-500">Each row is 36 months = 3 years</p>
+	<p class="text-orange-500">Each row is 36 months (3 years)</p>
 	<p>Months lived: {monthsLived}</p>
 	<p>Months remaining: {totalMonths - monthsLived}</p>
 </div>

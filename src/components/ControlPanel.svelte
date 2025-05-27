f69d47e6ac12c086021e21d3e94addd1b4a27bd8
Xurify <52548195+Xurify@users.noreply.github.com>
2025-05-27 14:26:39 -0400

    Updated styles

--- src/components/ControlPanel.svelte
+++ src/components/ControlPanel.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <div class="flex flex-col gap-4">
-	<div class="flex items-center">
+	<div class="flex items-center justify-center">
 		<Switch 
 			label="Advanced" 
 			bind:checked={showAdvanced} 
@@ -25,11 +25,12 @@
 			<Slider label="Age" bind:value={age} name="age" min={0} max={90} />
 		</div>
 
-		<div class="w-full max-w-md rounded-lg border border-white/20 bg-[#141612] p-4 lg:w-60">
+		<div class="w-full max-w-md rounded-lg border border-white/20 bg-[#141612] p-4 lg:w-68">
 			<span>Selected Month: </span>
 			<b>
 				{#if selectedMonthIndex}
 					{selectedMonthIndex + 1}
+					({Math.ceil((selectedMonthIndex + 1) / 12)} years)
 				{:else}
 					None
 				{/if}
